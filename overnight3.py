45bbd045573fce00d2966097de46c38ea0ad9cf4
Jasper Schelling <jasper@jasperschelling.com>
2016-12-11 12:05:08 +0100

    overnight experiments

---
+++ overnight3.py
@@ -0,0 +1,6 @@
+from experiment_framework import *;
+
+#runExperiment("randomsampling-moreno_names")
+runExperiment("pivot-RanDeg-moreno_names")
+runExperiment("pivot-MaxMin-moreno_names")
+runExperiment("pivot-MaxSum-moreno_names")
