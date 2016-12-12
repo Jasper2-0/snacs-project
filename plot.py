cf69e829b9e53c98abf266f82d54f192037a62d2
Jasper Schelling <jasper@jasperschelling.com>
2016-12-12 22:21:10 +0100

    plots!

---
+++ plot.py
@@ -0,0 +1,66 @@
+import os
+import csv
+import sys
+import getopt
+
+import numpy as np
+
+from utilities import *
+
+import cPickle as pickle
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+
+
+
+
+def main(argv):
+
+    resultName = ""
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"r:",["results"])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-r","--results"):
+            plot(arg)
+
+def plot(resultsName):
+
+    results = pickle.load(open(os.getcwd()+"/pickles/done/results-"+resultsName+".pickle", "rb"))
+
+    k = []
+    mae = []
+
+    for r in results:
+        k += [r['setup']['k']];
+        mae += [r['results']['MAE (for all estimates)']]
+    
+    k = np.asarray(k)
+    mae = np.array(mae)
+    
+    print k.max()
+    print mae.max()
+    
+    plt.scatter(k,mae,linewidths=0.0)
+    plt.grid()
+    plt.ylim(ymin=-1.0)
+    plt.ylim(ymax=5.0)
+    plt.xlim(xmin=0.0)
+    plt.xlim(xmax=6000)
+    plt.ylabel("MAE (for all estimates )")
+    plt.xlabel("k")
+    plt.xscale('symlog')
+    plt.title(resultsName)
+    plt.savefig("diagrams/"+resultsName+".pdf")
+    plt.clf();
+
+    
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ plotall.py
@@ -0,0 +1,23 @@
+from plot import *
+
+plot("randomsampling-dnc_corecipient")
+plot("randomsampling-ego_facebook")
+plot("randomsampling-moreno_names")
+plot("randomsampling-moreno_propro")
+plot("randomsampling-opsahl_powergrid")
+
+plot("pivot-MaxMin-dnc_corecipient")
+plot("pivot-MaxMin-ego_facebook")
+plot("pivot-MaxMin-moreno_names")
+plot("pivot-MaxMin-moreno_propro")
+
+plot("pivot-MaxSum-dnc_corecipient")
+plot("pivot-MaxSum-ego_facebook")
+plot("pivot-MaxSum-moreno_names")
+plot("pivot-MaxSum-moreno_propro")
+
+plot("pivot-RanDeg-dnc_corecipient")
+plot("pivot-RanDeg-ego_facebook")
+plot("pivot-RanDeg-moreno_names")
+plot("pivot-RanDeg-moreno_propro")
+plot("pivot-RanDeg-opsahl_powergrid")
