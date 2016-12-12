0341913ee25f5990553d9ee35b1f5687f03300fe
Jasper Schelling <jasper@jasperschelling.com>
2016-12-12 09:35:47 +0100

    overnight runners

--- overnight_dnc_corecipient.py
+++ overnight_dnc_corecipient.py
@@ -1,6 +1,6 @@
 from experiment_framework import *;
 
-#runExperiment("randomsampling-dnc_corecipient")
-#runExperiment("pivot-RanDeg-dnc_corecipient")
+runExperiment("randomsampling-dnc_corecipient")
+runExperiment("pivot-RanDeg-dnc_corecipient")
 runExperiment("pivot-MaxMin-dnc_corecipient")
 runExperiment("pivot-MaxSum-dnc_corecipient")
