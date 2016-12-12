a43a8455b34a4aaa393ff6380c2d18d8077fb078
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2016-12-12 19:44:38 +0100

    top_k experiment done (sort of)

--- top_k.py
+++ top_k.py
@@ -21,7 +21,7 @@ def getActualTopK(Graph,topk):
         avg_dists[i] = average
         i += 1
         
-    print "diameter: " + str(diameter)
+    #print "diameter: " + str(diameter)
     
     sorted_nodes = np.argsort(avg_dists)
     return sorted_nodes[0:topk] 
@@ -31,55 +31,84 @@ def topkExperiment(Graph, numberOfExperiments, topk):
     G = Graph
     # Calculate actual average distance for all nodes
     actualTopK = getActualTopK(G,topk)
-    print "actual top-" + str(topk) + ": " + str(actualTopK)
+    #print "actual top-" + str(topk) + ": " + str(actualTopK)
        
      
     """ some parameters """
     n = len(G.nodes())
-    alpha = 1.0             # alpha >= 1
-    L = np.arange(1,11)*5   # sample sizes    
+    diam_upper_bound = n
+    alpha = 0.15             # alpha >= 1 in theory, but yeah...
+    step = 100               # should depend on size of graph but since our graphs are roughly the same size
+    L = np.arange(1,11)*step # sample sizes (from 100 to 1000 atm)
     
-    """ RUN FOR DIFFERENT SIZES OF l """
-    for l in L:
-        error = alpha * np.sqrt(np.log2(n)/l)
-    
-        """ CALCULATE ESIMATE FOR INTITAL SAMPLE size l """
-        sample = np.random.choice(G.nodes(),l,replace=True)
-    
-        est_dists = np.zeros(n) # TODO: smarter, not reset at every iteration
+    candidate_sizes = np.zeros(len(L),dtype=np.int)
+    correctness_for_l = np.zeros(len(L))
+    """ <EXPERIMENT> """
+    for exp in range(0,numberOfExperiments):
+        sum_dists = np.zeros(n)
         
-        for u in sample:
-            lengths = nx.single_source_shortest_path_length(G,u)
-            for key, value in lengths.iteritems():
-                est_dists[key] += value
-    
-        # Average cummulative distances
-        est_dists = est_dists / l
-    
-        """ GET INITIAL CANDIDATE SET"""
-         # sort on distances
-        sorted_nodes = np.argsort(est_dists)
-        v_k = sorted_nodes[topk-1]
-        dist_k = est_dists[v_k]
-        bound = dist_k + error # TODO: multiply with (upper bound of) diameter of graph
-        is_candidate = np.zeros(n,np.bool)
-        for u in range (0,n):
-            if(est_dists[u] < bound):
-                is_candidate[u] = True
+        """ RUN FOR DIFFERENT SIZES OF l """
+        index_l = 0
+        for l in L:
+            error = alpha * np.sqrt(np.log10(n)/l)
         
-        """ EVALUATE """
-        print "sample set size:    " + str(l)
-        print "candidate set size: " + str(sum(is_candidate))
-        correctness = 0.0
-        for u in actualTopK:
-            if(is_candidate[u]):
-                correctness += 1
-        correctness /= topk
-        print "correctness: " + str(correctness)
+            """ CALCULATE ESIMATE FOR INTITAL SAMPLE size l """
+            sample = np.random.choice(G.nodes(),step,replace=True)
+            
+            for u in sample:
+                lengths = nx.single_source_shortest_path_length(G,u)
+                for key, value in lengths.iteritems():
+                    sum_dists[key] += value
+                # get longest path length for lower bound of diameter
+                longest = max(lengths.values())
+                if(2*longest < diam_upper_bound):
+                    diam_upper_bound = 2*longest
+        
+            # Average cummulative distances
+            est_dists = sum_dists / l
+        
+            """ GET INITIAL CANDIDATE SET"""
+             # sort on distances
+            sorted_nodes = np.argsort(est_dists)
+            v_k = sorted_nodes[topk-1]
+            dist_k = est_dists[v_k]
+            bound = dist_k + error*diam_upper_bound
+            is_candidate = np.zeros(n,np.bool)
+            for u in range (0,n):
+                if(est_dists[u] < bound):
+                    is_candidate[u] = True
+            
+            """ EVALUATE """
+            candidate_sizes[index_l] += sum(is_candidate)
+            correctness = 0.0
+            for u in actualTopK:
+                if(is_candidate[u]):
+                    correctness += 1
+            correctness /= topk
+            correctness_for_l[index_l] += correctness 
+            index_l += 1
+    """ </EXPERIMENT> """
+    
+    """ RESULTS """
+    candidate_sizes /= numberOfExperiments
+    correctness_for_l /= numberOfExperiments
+    results = np.empty(len(L),dtype=object)
+    for i in range(0,len(L)):
+        result = {}
+        result['sample size'] = L[i]
+        result['avg. candidate set size'] = candidate_sizes[i]
+        result['avg. correctness'] = correctness_for_l[i]
+        results[i] = result
+        
+    return results
     
     
-    
-    
-G = nx.
-topkExperiment(G, 1, 3)
-    
+#G = nx.karate_club_graph()
+#G = nx.convert_node_labels_to_integers(nx.davis_southern_women_graph())
+#G = nx.convert_node_labels_to_integers(nx.florentine_families_graph())
+#G = nx.gnp_random_graph(100,0.4)
+#G = nx.path_graph(10)
+#print topkExperiment(G, 20, 3)
+
+
+
